338f827ba28ac37a9fe82644cda8ef7998554d34
kristaponis <a.kristaponis@icloud.com>
2021-01-04 15:26:54 +0200

    Update

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"github.com/go-chi/chi"
 	"go-chi-rest/handlers"
 	"log"
 	"net/http"
 )
 
+const (
+	PORT = ":8080"
+)
+
 func main() {
 	r := chi.NewRouter()
 
-	r.Get("/", handlers.PostsHandler)
-	r.Post("/", handlers.PostsHandler)
+	r.Get("/posts", handlers.PostsHandler)
+	r.Post("/posts", handlers.PostsHandler)
 
-	if err := http.ListenAndServe(":8080", r); err != nil {
+	fmt.Println("Serving application...")
+	if err := http.ListenAndServe(PORT, r); err != nil {
 		log.Fatal(err)
 	}
 }
